5f4dc3fba2270ae66e3cf2ddf2c11e0db8f209bc
Enis <xmarks.dev@gmail.com>
2024-07-02 16:32:44 +0200

    1.0.1 - updated README

--- pc-security.php
+++ pc-security.php
@@ -16,7 +16,7 @@
  * Plugin Name:       ProCod Security
  * Plugin URI:        https://https://procoders.tech/
  * Description:       This is a description of the plugin.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            ProCoders
  * Author URI:        https://https://procoders.tech//
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-const PC_SECURITY_VERSION = '1.0.0';
+const PC_SECURITY_VERSION = '1.0.1';
 
 /**
  * The code that runs during plugin activation.
